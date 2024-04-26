be566dc184d8dd84f570d511e6a823c5a6f8d9e3
Anandraj2501 <araj250101@gmail.com>
2024-04-26 14:07:15 +0530

    Resolver Port error

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 5000
+const PORT = process.env.PORT || 4000;
 const {mongoURl} = require("./utils/Keys.js");
 const multer = require("multer");
 const cors = require("cors");
